5fd189993f5a39d8885fa97b4c9060fa25e0d30e
Yassine Khefifi <yassinekhefifi.student@gmail.com>
2025-10-17 15:09:41 +0100

    generation du jar

--- src/main/java/com/example/ams_sesame/Entities/Provider.java
+++ src/main/java/com/example/ams_sesame/Entities/Provider.java
@@ -18,7 +18,7 @@ import java.io.Serializable;
 @Entity
 public class Provider implements Serializable {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
     private String name;
     private String email;
